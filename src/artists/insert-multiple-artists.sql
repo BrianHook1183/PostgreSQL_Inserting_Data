c147332ed7a6d1f203e427b89cf6c5d6092ee8f4
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-05-22 04:24:18 -0500

    submitted solution

---
+++ src/artists/insert-multiple-artists.sql
@@ -0,0 +1,5 @@
+INSERT INTO artists (first_name, last_name, birthday, is_alive)
+VALUES
+  ('brian', 'hook', '1989-02-28', true),
+  ('lucas', 'konopka', '1988-02-28', true),
+  ('andrew', 'byrd', '1987-02-28', true);
